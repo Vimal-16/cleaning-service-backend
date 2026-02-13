3ee760e637c24376b1ccc46c568c21a0b4cba240
Vimal-16 <vimalvelusamy16@gmail.com>
2026-02-13 21:00:54 +0530

    Fix CORS for Vercel

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ app.use(cors({
     'http://localhost:5173',
     'https://cleaning-service-frontend1664.vercel.app'
   ],
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
